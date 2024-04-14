e08c64b8139b7de5d093613bd1b53de6125c1e1d
wlkkt <yangyf0618@qq.com>
2024-04-14 23:31:48 +0800

    完善

--- daily-test/test.cpp
+++ daily-test/test.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
 ////////////////////////////一、vector的迭代器失效
-//int main()
+//int main()//1 2 3 4 5 6 7 8 9
 //{
 //	int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
 //	int n = sizeof(ar) / sizeof(int);
@@ -28,6 +29,23 @@ using namespace std;
 //}
 
 
+//int main()//1 2 3 4 6 7 8 9
+//{
+//	int array[] = { 1, 2, 3, 4, 0, 5, 6, 7, 8, 9 };
+//	int n = sizeof(array) / sizeof(int);
+//	list<int> mylist(array, array + n);
+//	auto it = mylist.begin();
+//	while (it != mylist.end())
+//	{
+//		if (*it != 0)
+//			cout << *it << " ";
+//		else
+//			it = mylist.erase(it);
+//		++it;
+//	}
+//	return 0;
+//}
+
 
 //////////////////////////////二、显示 / 隐式调用构造
 
@@ -50,3 +68,57 @@ using namespace std;
 //}
 
 ////////////////////////////
+
+//int main()
+//{
+//	int ar[] = { 0,1, 2, 3, 4,  5, 6, 7, 8, 9 };
+//	int n = sizeof(ar) / sizeof(int);
+//	list<int> mylist(ar, ar + n);
+//	list<int>::iterator pos = find(mylist.begin(), mylist.end(), 5);
+//	reverse(mylist.begin(), pos);
+//	reverse(pos, mylist.end());
+//	list<int>::const_reverse_iterator crit = mylist.crbegin();
+//	while (crit != mylist.crend())
+//	{
+//		cout << *crit << " ";
+//		++crit;
+//	}
+//	cout << endl;
+//	return 0;
+//}
+//
+class Solution 
+{
+public:
+    string tmp;//字符串尾插
+    vector<string> res;//将尾插好的字符串成组尾插给res
+    vector<string> board = { "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
+    void DFS(int pos, string digits)//深度优先遍历
+    {
+        if (pos == digits.size())//当digits的每个有效字符都递归并尾插后返回
+        {
+            res.push_back(tmp);
+            return;
+        }
+
+        int num = digits[pos] - '0';
+
+        for (int i = 0; i < board[num].size(); i++)
+        {
+            tmp.push_back(board[num][i]);
+            DFS(pos + 1, digits);
+            tmp.pop_back();//删除
+        }
+    }
+
+ 
+        vector<string> letterCombinations(string digits)
+        {
+            if (digits.size() == 0)
+            {
+                return res;
+            }
+            DFS(0, digits);
+            return res;
+        }
+};
