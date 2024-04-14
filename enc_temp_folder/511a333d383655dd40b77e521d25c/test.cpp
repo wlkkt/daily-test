83aa362f46cc89aafb1564de7bd3a197c2c7aad4
wlkkt <yangyf0618@qq.com>
2024-04-14 21:37:28 +0800

    添加项目文件。

---
+++ enc_temp_folder/511a333d383655dd40b77e521d25c/test.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+////////////////////////////一、
+//int main()
+//{
+//	int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
+//	int n = sizeof(ar) / sizeof(int);
+//	vector<int> v(ar, ar + n);
+//	vector<int>::iterator it = v.begin();
+//	while (it != v.end())
+//	{
+//		if (*it != 0)
+//		{
+//			cout << *it;
+//			++it;
+//		}
+//	
+//		else
+//		{
+//			it = v.erase(it);
+//		}
+//		
+//		
+//	}
+//	return 0;
+//}
+
+
+
+//////////////////////////////二、
+
+class Base {
+public:
+    Base() {
+        cout << "Base constructor called." << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() { // 派生类未显式调用基类构造函数
+        cout << "Derived constructor called." << endl;
+    }
+};
+
+int main() {
+    Derived d;  // 创建派生对象
+}
